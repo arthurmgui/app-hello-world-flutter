ce461b10e727955bde17c12449886c5c470ce42b
arthurmgui <arthurmottaguimaraes@outlook.com>
2022-06-09 21:50:54 -0300

    Aprendendo a usar Scaffold e Row.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-      title: "Hello, World!",
-      home: Container(
-          color: Colors.white,
-          child: Column(
-            children: [
-              Text("Hello, World!"),
-            ],
-          ))));
+    title: "Hello, World!",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Row(
+          children: [
+            Text("Hello, World!"),
+          ],
+        ),
+      ),
+    ),
+  ));
 }
